30df676fae5736f0970f8a0d8073221270172408
Cabrel <idrisscabrel53@gmail.com>
2025-09-01 12:51:34 +0200

    creation des migrations,models,requests,controllers et routes

---
+++ laravel/database/migrations/2025_09_01_101728_create_trajets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trajets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('lieu_depart');
+            $table->string('lieu_arrivee');
+            $table->date('date_trajet');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trajets');
+    }
+};
