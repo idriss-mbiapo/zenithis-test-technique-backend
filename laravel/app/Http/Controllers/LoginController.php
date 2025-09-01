d43ef5d68d91f973d38948056d3f2ad1b3d59ca4
Cabrel <idrisscabrel53@gmail.com>
2025-09-01 13:34:39 +0200

    creation des routes (login,ajout,consultation,mis a jour, suppression des trajets et Fonctionnalite authentification: Login + JWT

--- laravel/app/Http/Controllers/LoginController.php
+++ laravel/app/Http/Controllers/LoginController.php
@@ -1,10 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\LoginRequest;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['message' => 'Email ou mot de passe incorrect.'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['message' => 'Erreur lors de la création du token.'], 500);
+        }
+
+        $user = JWTAuth::user();
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => JWTAuth::factory()->getTTL() * 60,
+            'user' => $user,
+        ]);
+    }
 }
